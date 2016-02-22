116a040c13837afa60aa4d4dacde90950321dfd2
shelbyedmonds <sce60786@uga.edu>
2016-02-21 23:08:54 -0500

    third commit

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -5,33 +5,39 @@ public class HumanPlayer extends Player{
 	private int numOfPieces;
 	
 	public HumanPlayer(){
-	
+		
 	
 	}//constructor
 	
+	@override
 	public int getTurnNum(){
 
 	return turn;  //taken from parent class (protected modifier)
 	}//getTurnNum
-
+	
+	@override
 	public boolean moveValidation(){
 
 	return false;
 	}//moveValidation
-
+	
+	@override
 	public ReversiBoard playersMove(){
 	return ReversiBoard;
 	}//playersMove
 
+	@override
 	public boolean isYourTurn(){
 	return false;
 	}//isYourTurn
-
+	
+	@override
 	public ReversiBoard changeBoard(){
 
 	return ReversiBoard;
 	}//ReversiBoard
 
+	@override
 	public void setTurn(){
 
 	}//setTurn
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,26 @@
 public abstract class Player{
+	//attributes 
+	protected int turn;
 
 
 
+/** SHOULD WE ADD IN A CONSTRUCTOR?*/
+	public boolean moveValidation(){
+	}//moveValidation
+	
+	public ReversiBoard playersMove(){
+	}//playersMove
+	
+	public boolean isYourTurn(){
+	}//isYourTurn
+	
+	public ReversiBoard changeBoard(){
+	}//changeBoard
+
+	public int getTurnNum(){
+	}//getTurnNum
+
+	public void setTurn(){
+	}//setTurn
 
 }//Player
