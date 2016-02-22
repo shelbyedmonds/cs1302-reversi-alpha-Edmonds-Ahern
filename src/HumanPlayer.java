2cd2c09856996efb3bf88f0aa8c9695520e8ef68
shelbyedmonds <sce60786@uga.edu>
2016-02-21 23:01:26 -0500

    second commit

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,6 +1,40 @@
 public class HumanPlayer extends Player{
+	//attributes
+	private int row;
+	private int column;
+	private int numOfPieces;
+	
+	public HumanPlayer(){
+	
+	
+	}//constructor
+	
+	public int getTurnNum(){
 
+	return turn;  //taken from parent class (protected modifier)
+	}//getTurnNum
 
+	public boolean moveValidation(){
+
+	return false;
+	}//moveValidation
+
+	public ReversiBoard playersMove(){
+	return ReversiBoard;
+	}//playersMove
+
+	public boolean isYourTurn(){
+	return false;
+	}//isYourTurn
+
+	public ReversiBoard changeBoard(){
+
+	return ReversiBoard;
+	}//ReversiBoard
+
+	public void setTurn(){
+
+	}//setTurn
 
 
 
