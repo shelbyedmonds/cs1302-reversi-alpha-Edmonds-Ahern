863b496ecb3cd9406e5736c9b87362c741335ca5
shelbyedmonds <sce60786@uga.edu>
2016-02-25 13:43:17 -0500

    update at 1:43 pm

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -41,7 +41,168 @@ public abstract class ComputerPlayer extends Player{
 	}//moveValidation
 
 	@override
-	public ReversiBoard changeBoard(){
+	public ReversiBoard changeBoard(String playersToken, Board ReversiBoard){
+		moveValidation(ReversiBoard);
+		ReversiBoard[randomRow][randomColumn]=playersToken;
+		ReversiBoard[row][column]=playersToken;
+                boolean changeToLeft=false;
+                boolean changeToRight=false;
+                boolean changeUp=false;
+                boolean changeDown=false;
+                int lastRightIndexToChange=100;
+                int lastLeftIndexToChange=-1;
+                int lastUpIndexToChange;
+                int lastDownIndexToChange;
+                boolean changeLeftUp=false;
+                int lastLeftUpRowIndex=-1;
+                int lastLeftUpColumnIndex=-1;
+                boolean changeRightDown=false;
+                int lastRightDownRowIndex=100;
+                int lastRightDownColumnIndex=100;
+                boolean changeRightUp=false;
+                int lastRightUpRowIndex=-1;
+                int lastRightUpColumnIndex=100;
+                boolean changeLeftDown=false;
+                int lastLeftDownRowIndex=100;
+                int lastLeftDownColumnIndex=-1;
+
+		//horizontal right loop changer 
+		for(int j=randomColumn; j<8; j++){
+                        if(ReversiBoard[randomRow][j].equals("_"){
+                                lastRightIndexToChange=j;
+                                changeToRight=true;
+                                break;
+                        }
+                        if(changeToRight==true){
+                                for(int q=randomColumn; q<=lastRightIndexToChange; q++){
+                                        ReversiBoard[randomRow][q]=playersToken;
+                                }
+                        }
+                lastRightIndexToChange=100;
+                changeToRight=false;
+                }
+
+		//horizontal left loop changer
+		for(int j=randomColumn; j>0; j--){
+                        if(ReversiBoard[randomRow][j].equals("_"){
+                                lastLeftIndexToChange=j;
+                                changeToLeft=true;
+                                break;
+                        }
+                        if(changeToLeft=true){
+                                for(int q=randomColumn; q>=lastLeftIndexToChange; q--){
+                                        ReversiBoard[randomRow][q]=playersToken;
+                                }
+                        }
+                changeToLeft=false;
+                lastLeftIndexToChange=-1;
+                }
+		
+		//vertical up loop changer
+		for(int i=randomRow; i>0; i--){
+                        if(ReversiBoard[i][randomColumn].equals("_"){
+                                lastUpIndexToChange=i;
+                                changeUp=true;
+                                break;
+                        }
+                        if(changeUp=true){
+                                for(int t=randomRow; t>=lastUpIndexToChange; t--){
+                                        ReversiBoard[t][randomColumn]=playersToken;
+                                }
+                        }
+                changeUp=false;
+                lastUpIndexToChange=-1;
+                }
+
+		//vertical down loop changer
+		for(int i=randomRow; i<8; i++){
+                        if(ReversiBoard[i][randomColumn].equals("_"){
+                                lastDownIndexToChange=i;
+                                changeDown=true;
+                                break;
+                        }
+                        if(changeDown=true){
+                                for(int t=randomRow; t<=lastUpIndexToChange; t++){
+                                        ReversiBoard[t][randomColumn]=playersToken;
+                                }
+                        }
+                changeDown=false;
+                lastDownIndexToChange=100;
+                }
+
+		//diagonal left up loop changer
+		for(int i=randomRow, j=randomColumn; i>0&&j>0; j--, i--){
+                        if(ReversiBoard[i][j].equals("_"){
+                                lastLeftUpRowIndex=i;
+                                lastLeftUpColumnIndex=j;
+                                changeLeftUp=true;
+                                break;
+                        }
+                        if(changeLeftUp=true){
+                                for(int q=randomRow, t=randomColumn; q>=lastLeftUpRowIndex, t>=lastLeftUpColumnIndex; q--, t--){
+                                                ReversiBoard[q][t]=playersToken;
+                                }
+                        }
+                        changeLeftUp=false;
+                        lastLeftUpRowIndex=-1;
+                        lastLeftUpColumnIndex=-1;
+                }
+		//diagonal right down loop changer
+		for(int i=randomRow, j=randomColumn; i>0&&j<8; j++, i--){
+                        if(ReversiBoard[i][j].equals("_"){
+                                lastRightUpRowIndex=i;
+                                lastRightUpColumnIndex=j;
+                                changeRightUp=true;
+                                break;
+                        }
+                        if(changeRightUp=true){
+                                for(int q=randomRow, t=randomColumn; q>=lastRightDownRowIndex, t<=lastRightDownColumnIndex; q--, t++){                                     
+                                        ReversiBoard[q][t]=playersToken;
+                                }
+                        }
+                        changeRightUp=false;
+                        lastRightUpRowIndex=-1;
+                        lastRightUpColumnIndex=100;
+                }
+
+		//diagonal right up loop changer
+		for(int i=randomRow, j=randomColumn; i>0&&j<8; j++, i--){
+                        if(ReversiBoard[i][j].equals("_"){
+                                lastRightUpRowIndex=i;
+                                lastRightUpColumnIndex=j;
+                                changeRightUp=true;
+                                break;
+                        }
+                        if(changeRightUp=true){
+                                for(int q=randomRow, t=randomColumn; q>=lastRightDownRowIndex, t<=lastRightDownColumnIndex; q--, t++){                                     
+                                        ReversiBoard[q][t]=playersToken;
+                                }
+                        }
+                        changeRightUp=false;
+                        lastRightUpRowIndex=-1;
+                        lastRightUpColumnIndex=100;
+                }
+
+		//diagonal left down loop changer
+		for(int i=randomRow, j=randomColumn; i<8&&j>0; j--, i++){
+                        if(ReversiBoard[i][j].equals("_"){
+                                lastLeftDownRowIndex=i;
+                                lastLeftDownColumnIndex=j;
+                                changeLeftDown=true;
+                                break;
+                        }
+                        if(changeLeftDown=true){
+                                for(int q=randomRow, t=randomColumn; q<=lastLeftUpRowIndex, t>=lastLeftUpColumnIndex; q++, t--){
+                                                ReversiBoard[q][t]=playersToken;
+                                }
+                        }
+                        changeLeftDown=false;
+                        lastLeftDownRowIndex=100;
+                        lastLeftDownColumnIndex=-1;
+                }
+
+
+		return ReversiBoard;
 	}//changeBoard
 
 
--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -36,19 +36,166 @@ public class HumanPlayer extends Player{
 
 	@override
 	public ReversiBoard changeBoard(String playersToken, int row, int column, Board ReversiBoard){
-		int changeToRow=0;
-		int changeToColumn=0;
-		boolean vertical=false;
-		boolean horizontal=false;
-		boolean diagonal=false;
-		ReversiBoard[row][column]=" "+ playersToken+ " ";
+		ReversiBoard[row][column]=playersToken;
+		boolean changeToLeft=false;
+		boolean changeToRight=false;
+		boolean changeUp=false;
+		boolean changeDown=false;
+		int lastRightIndexToChange=100;
+		int lastLeftIndexToChange=-1;
+		int lastUpIndexToChange;
+		int lastDownIndexToChange;	
+		boolean changeLeftUp=false;
+                int lastLeftUpRowIndex=-1;
+                int lastLeftUpColumnIndex=-1;
+		boolean changeRightDown=false;
+                int lastRightDownRowIndex=100;
+                int lastRightDownColumnIndex=100;
+		boolean changeRightUp=false;
+                int lastRightUpRowIndex=-1;
+                int lastRightUpColumnIndex=100;
+		boolean changeLeftDown=false;
+                int lastLeftDownRowIndex=100;
+                int lastLeftDownColumnIndex=-1;
 		
-		//horizontal loop changer
+		//horizontal right loop changer
+		for(int j=column; j<8; j++){
+			if(ReversiBoard[row][j].equals("_"){
+				lastRightIndexToChange=j;
+				changeToRight=true;
+				break;
+			}
+			if(changeToRight==true){
+				for(int q=column; q<=lastRightIndexToChange; q++){
+					ReversiBoard[row][q]=playersToken;
+				}
+			}
+		lastRightIndexToChange=100;
+		changeToRight=false;
+		}
+		//horizontal left loop changer
+		for(int j=column; j>0; j--){
+			if(ReversiBoard[row][j].equals("_"){
+				lastLeftIndexToChange=j;
+				changeToLeft=true;
+				break;
+			}
+			if(changeToLeft=true){
+				for(int q=column; q>=lastLeftIndexToChange; q--){
+					ReversiBoard[row][q]=playersToken;
+				}
+			}
+		changeToLeft=false;
+		lastLeftIndexToChange=-1;
+		}
+		//vertical up loop changer
+		for(int i=row; i>0; i--){
+			if(ReversiBoard[i][column].equals("_"){
+				lastUpIndexToChange=i;
+				changeUp=true;
+				break;
+			}
+			if(changeUp=true){
+				for(int t=row; t>=lastUpIndexToChange; t--){
+					ReversiBoard[t][column]=playersToken;
+				}
+			}
+		changeUp=false;
+		lastUpIndexToChange=-1;
+		}
+
+		//vertical down checker
+		for(int i=row; i<8; i++){
+                        if(ReversiBoard[i][column].equals("_"){         
+                                lastDownIndexToChange=i;
+                                changeDown=true;
+                                break;
+                        }
+                        if(changeDown=true){
+                                for(int t=row; t<=lastUpIndexToChange; t++){
+                                        ReversiBoard[t][column]=playersToken;
+                                }
+                        }
+                changeDown=false;
+                lastDownIndexToChange=100;
+                }
+				
+
+
+		//diagonal left up loop changer
+		for(int i=row, j=column; i>0&&j>0; j--, i--){		
+			if(ReversiBoard[i][j].equals("_"){
+				lastLeftUpRowIndex=i;
+				lastLeftUpColumnIndex=j;
+				changeLeftUp=true;
+				break;
+			}
+			if(changeLeftUp=true){
+				for(int q=row, t=column; q>=lastLeftUpRowIndex, t>=lastLeftUpColumnIndex; q--, t--){
+						ReversiBoard[q][t]=playersToken;
+				}
+			}
+			changeLeftUp=false;
+			lastLeftUpRowIndex=-1;
+			lastLeftUpColumnIndex=-1;
+		}
+
+		//diagonal right down loop changer
 		
-		//vertical loop changer
+		   for(int i=row, j=column; i<8&&j<8; j++, i++){
+                        if(ReversiBoard[i][j].equals("_"){                      
+                                lastRightDownRowIndex=i;
+                                lastRightDownColumnIndex=j;
+                                changeRightDown=true;
+                                break;
+                        }
+                        if(changeRightDown=true){
+                                for(int q=row, t=column; q<=lastRightDownRowIndex, t<=lastRightDownColumnIndex; q++, t++){                                                                      
+					ReversiBoard[q][t]=playersToken;
+                                
+				}
+                        }
+                        changeRightDown=false;
+                        lastRightDownRowIndex=100;
+                        lastRightDownColumnIndex=100;
+                }
+
+		//diagonal right up loop checker
+
+		  for(int i=row, j=column; i>0&&j<8; j++, i--){
+                        if(ReversiBoard[i][j].equals("_"){
+                                lastRightUpRowIndex=i;
+                                lastRightUpColumnIndex=j;
+                                changeRightUp=true;
+                                break;
+                        }
+                        if(changeRightUp=true){
+                                for(int q=row, t=column; q>=lastRightDownRowIndex, t<=lastRightDownColumnIndex; q--, t++){                                                                      
+					ReversiBoard[q][t]=playersToken;
+                                }
+                        }
+                        changeRightUp=false;
+                        lastRightUpRowIndex=-1;
+                        lastRightUpColumnIndex=100;
+                }
+		//diagonal left down loop checker
 		
-		//diagonal loop changer
-					
+		for(int i=row, j=column; i<8&&j>0; j--, i++){
+                        if(ReversiBoard[i][j].equals("_"){
+                                lastLeftDownRowIndex=i;
+                                lastLeftDownColumnIndex=j;
+                                changeLeftDown=true;
+                                break;
+                        }
+                        if(changeLeftDown=true){
+                                for(int q=row, t=column; q<=lastLeftUpRowIndex, t>=lastLeftUpColumnIndex; q++, t--){
+                                                ReversiBoard[q][t]=playersToken;
+                                }
+                        }
+                        changeLeftDown=false;
+                        lastLeftDownRowIndex=100;
+                        lastLeftDownColumnIndex=-1;
+                }				
 				
 		
 	return ReversiBoard;
